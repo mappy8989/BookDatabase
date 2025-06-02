cd9bf2dc37c9a634fa0695089251f1d8679bfba9
IgorF <kleef@mail.ru>
2025-06-02 13:42:45 +0300

    Fill BookDatabase class

--- include/book_database.hpp
+++ include/book_database.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <initializer_list>
 #include <print>
 #include <string>
 #include <string_view>
@@ -15,12 +16,22 @@ template <BookContainerLike BookContainer = std::vector<Book>>
 class BookDatabase {
 public:
     // Type aliases
-
+    using value_type = Book;
+    using reference = value_type &;
+    using const_reference = const value_type &;
+    using pointer = value_type *;
+    using const_pointer = const value_type *;
+    using iterator = BookContainer::iterator;
+    using const_iterator = BookContainer::const_iterator;
+    using difference_type = std::ptrdiff_t;
+    using size_type = std::size_t;
     // Ваш код здесь
 
-    using AuthorContainer = BookContainer /* Ваш код здесь */;
+    using AuthorContainer = std::vector<std::string>;
 
-    BookDatabase() = default;
+    BookDatabase(std::initializer_list<Book> list) {
+        books_.insert(BooksBegin(), list.begin(), list.end());
+    }
 
     void Clear() {
         books_.clear();
@@ -28,8 +39,15 @@ public:
     }
 
     // Standard container interface methods
+    iterator BooksBegin() { return books_.begin(); }
+    iterator BooksEnd() { return books_.end(); }
 
+    reference EmplaceBack(Book &&book) { return books_.emplace_back(book); }
+    void PushBack(Book &book) { books_.push_back(book); }
     // Ваш код здесь
+
+    const BookContainer &GetBooks(void) { return books_; }
+    const AuthorContainer &GetAuthors(void) { return authors_; }
 
 private:
     BookContainer books_;
@@ -42,10 +60,11 @@ namespace std {
 template <>
 struct formatter<bookdb::BookDatabase<std::vector<bookdb::Book>>> {
     template <typename FormatContext>
-    auto format(const bookdb::BookDatabase<std::vector<bookdb::Book>> &db, FormatContext &fc) const {
+    auto format(const bookdb::BookDatabase<std::vector<bookdb::Book>> &db,
+                FormatContext &fc) const {
         /*
-        Раскомментируйте, когда bookdb::BookDatabase поддержит интерфейсы, доступные стандартным контейнерам
-        (size/begin/...)
+        Раскомментируйте, когда bookdb::BookDatabase поддержит интерфейсы, доступные стандартным
+        контейнерам (size/begin/...)
 
         format_to(fc.out(), "BookDatabase (size = {}): ", db.size());
 
