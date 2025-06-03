decf60e591bb3fa34220c610a9ea1304072539b4
IgorF <kleef@mail.ru>
2025-06-03 11:18:37 +0300

    DB tests fix

--- tests/db_test.cpp
+++ tests/db_test.cpp
@@ -4,23 +4,25 @@
 using namespace bookdb;
 
 TEST(BooksDataBase, PushBackCheck) {
-    Book bk(Genre::Fiction);
+    Book bk("1984", "George Orwell", 1949, Genre::SciFi, 4., 190);
     BookDatabase bdb;
 
     bdb.PushBack(bk);
-    EXPECT_EQ(bdb.GetBooks().at(0).genre, Genre::Fiction);
+    EXPECT_EQ(bdb.GetBooks().at(0).genre, Genre::SciFi);
 }
 
 TEST(BooksDataBase, EmplaceBackCheck) {
-    Book bk(Genre::Fiction);
     BookDatabase bdb;
 
-    bdb.EmplaceBack({"NonFiction"});
-    EXPECT_EQ(bdb.GetBooks().at(0).genre, Genre::NonFiction);
+    bdb.EmplaceBack({"William Golding", "Lord of the Flies", 1954, Genre::Fiction, 4.2, 89});
+    EXPECT_EQ(bdb.GetBooks().at(0).genre, Genre::Fiction);
 }
 
 TEST(BooksDataBase, InitializerListCheck) {
-    Book bk1(Genre::Fiction), bk2(Genre::NonFiction), bk3(Genre::Mystery);
+    Book bk1("1984", "George Orwell", 1949, Genre::SciFi, 4., 190);
+    Book bk2("Animal Farm", "George Orwell", 1945, Genre::Fiction, 4.4, 143);
+    Book bk3("The Great Gatsby", "F. Scott Fitzgerald", 1925, Genre::Fiction, 4.5, 120);
+
     BookDatabase bdb = {bk1, bk2, bk3};
-    EXPECT_EQ(bdb.GetBooks().at(2).genre, Genre::Mystery);
+    EXPECT_EQ(bdb.GetBooks().at(2).genre, Genre::Fiction);
 }
