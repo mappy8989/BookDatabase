6878f7b7b18268386eea1f4a014ebea3d74bfbc6
IgorF <kleef@mail.ru>
2025-06-04 09:47:21 +0300

    Add concept

--- include/concepts.hpp
+++ include/concepts.hpp
@@ -8,20 +8,27 @@
 namespace bookdb {
 
 template <typename T>
-concept BookContainerLike =
-    requires(T &t, typename T::value_type v) { t.push_back(v); } && std::same_as<typename T::value_type, bookdb::Book>;
+concept BookContainerLike = requires(T &t, typename T::value_type v) { t.push_back(v); } &&
+                            std::same_as<typename T::value_type, bookdb::Book>;
 
 template <typename T>
 concept BookIterator = std::random_access_iterator<T> &&  // рандом-доступный итератор
                        std::same_as<typename std::iterator_traits<T>::value_type, bookdb::Book>;
 
 template <typename S, typename I>
-concept BookSentinel = true;
+concept BookSentinel = requires(S s, I i) {
+    { s == i } -> std::convertible_to<bool>;
+    { i == s } -> std::convertible_to<bool>;
+};
 
-template <typename P>
-concept BookPredicate = true;
+template <typename P, typename T>
+concept BookPredicate = requires(P pred, T &&val) {
+    { pred(std::forward<T>(val)) } -> std::convertible_to<bool>;
+};
 
-template <typename C>
-concept BookComparator = true;
+template <typename C, typename T>
+concept BookComparator = requires(C comp, T a, T b) {
+    { comp(a, b) } -> std::convertible_to<bool>;
+};
 
 }  // namespace bookdb
